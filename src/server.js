459a14e31d0627d047ee8272aa3b9e09d3079851
DarlanDuarte <darlan.duarte.m@gmail.com>
2022-11-14 15:19:29 -0300

    Estrutura Inicial

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log('ok')
+const express = require('express')
+//const path = require('path')
+
+//const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+// conexão com banco de dados
+//db.connect()
+
+
+// habilita server para receber dados via post (formulario)
+app.use(express.urlencoded({extended: true}))
+
+//definindo as rotas
+app.use('/api', routes)
+
+
+// executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
