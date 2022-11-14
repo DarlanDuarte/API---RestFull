595e846b7c5f76c2e9530a28f9f41a25747ae08c
DarlanDuarte <darlan.duarte.m@gmail.com>
2022-11-14 16:49:19 -0300

    Adicionando model e Controller

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router()
 
+// VERBOS HTTP (4 TIPOS)
+// GET - Obter dados
+// POST - Enviar/Receber dados
+// PUT - Atualizar dados
+// DELETE - Remover Dados
+
+const ProductsController = require('../controllers/products')
+
+router.get('/products', ProductsController.get)
+//router.post('/products', ProductsController.post)
+//router.put('/products', ProductsController.put)
+//router.delete('/products', ProductsController.delete)
+
+
+
+
 router.get('/clientes', (req, res) => {
     res.send({
         ok: 123
