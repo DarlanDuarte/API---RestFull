f4b2d837d6c83c14d45718ba22e16fd89e60becd
DarlanDuarte <darlan.duarte.m@gmail.com>
2022-11-15 14:51:05 -0300

    Adicionando metodo PUT

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,7 +11,7 @@ const ProductsController = require('../controllers/products')
 // id? = parametro opcional
 router.get('/products/:id?', ProductsController.get)
 router.post('/products', ProductsController.post)
-//router.put('/products', ProductsController.put)
+router.put('/products/:id', ProductsController.put)
 //router.delete('/products', ProductsController.delete)
 
 
