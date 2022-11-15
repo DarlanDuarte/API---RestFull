2125739c8bbf5a03a8a4a2f465893d9b033c206f
DarlanDuarte <darlan.duarte.m@gmail.com>
2022-11-15 15:27:10 -0300

    Adicionando Delete-Remove

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -64,8 +64,32 @@ async function put(req, res){
     }) */
 }
 
+async function remove(req, res){
+    
+    const { id } = req.params
+
+    const remove = await ProductsModel.deleteOne({_id : id})
+
+    const message = remove ? 'success' : 'error'
+
+    /* 
+    if(!remove.ok){
+        menssagem = 'error'
+    }
+    */
+
+    res.send({
+        message,
+    })
+
+
+}
+
+
+
 module.exports = {
     get,
     post,
     put, 
+    remove,
 } 
